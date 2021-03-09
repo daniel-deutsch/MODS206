f34e78a876a5b5ca6968401942eb9418d1f5c77d
daniel-deutsch <danieljorged1@gmail.com>
2021-03-09 17:12:18 +0100

    details

--- collect.py
+++
@@ -1,9 +0,0 @@
-from requests_html import HTMLSession
-
-
-session = HTMLSession()
-r = session.get('http://python-requests.org')
-
-r.html.render()
-
-print(r.html.search('Python 2 will retire in only {months} months!')) # None
